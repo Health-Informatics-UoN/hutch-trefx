9387e13e02dc9d491732e342b6ceda7517bf9549
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-02 16:39:10 +0100

    https config class
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

---
+++ app/DummyControllerApi/Constants/HttpsConfig.cs
@@ -0,0 +1,9 @@
+namespace DummyControllerApi.Constants;
+
+public class HttpsConfig
+{
+  /// <summary>
+  /// Disable HTTPS redirection for the app.
+  /// </summary>
+  public bool DisableHttpsRedirection { get; set; }
+}
